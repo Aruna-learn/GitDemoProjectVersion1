9110e224682b65c04ea20c6924fb7a5d0f0fa6f2
Aruna Reddy tech <arunaammu9922@gmail.com>
2024-05-22 10:25:22 +0530

    My first commit as TL

---
+++ src/com/git/TLGitPractise.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class TLGitPractise {
+
+	public static void main(String[] args) {
+		int i=90;
+		if(i<60)
+		{
+			System.out.println("Greater number");
+		}
+		else
+		{
+			System.out.println("negative number");
+		}
+		
+	}
+	}
+
+
